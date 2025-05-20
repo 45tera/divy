a748458b39ee4f3d394c5f99bc3c8f73fbfd989e
45tera <45terax@gmail.com>
2025-05-20 16:03:15 +0800

    add: photo from gallery option

--- lib/ScannerScreen.dart
+++ lib/ScannerScreen.dart
@@ -1,7 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ScannerScreen extends StatelessWidget {
+class ScannerScreen extends StatefulWidget {
+  @override
+  ScannerScreenState createState() => ScannerScreenState();
+}
+
+class ScannerScreenState extends State<ScannerScreen> {
+  Image? imageToOCR;
+  final ImagePicker picker = ImagePicker();
+
+  Future pickImage() async {
+    final selectedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (selectedImage == null) return;
+
+    setState(() {
+      imageToOCR = Image.network(selectedImage.path); // For web
+    });
+  }
+
+  Future captureImage() async {
+    final XFile? selectedPhoto = await picker.pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (selectedPhoto == null) return null;
+
+    setState(() {
+      //imageToOCR = XFile(selectedPhoto!.path);
+      imageToOCR = Image.network(selectedPhoto.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +45,17 @@ class ScannerScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Center(child: Text('Scan the QR')),
-            Center(child: Container(color: Colors.blue)),
+            MaterialButton(
+              color: Colors.blue,
+              child: Text("Grab receipt photo from gallery"),
+              onPressed: () {
+                pickImage();
+              },
+            ),
+            Center(
+              child:
+                  imageToOCR ?? Text('No image selected'),
+            ),
           ],
         ),
       ),
